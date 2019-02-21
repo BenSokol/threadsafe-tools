6db893f38e7988783b8ea1cb89b45c3b186885f0
Ben Sokol <ben@bensokol.com>
2019-02-21 00:47:06 -0600

    Add flush to print calls and void call function

--- TS_call.hpp
+++ TS_call.hpp
@@ -3,7 +3,7 @@
 * @Author:   Ben Sokol <Ben>
 * @Email:    ben@bensokol.com
 * @Created:  February 15th, 2019 [2:36pm]
-* @Modified: February 19th, 2019 [12:54am]
+* @Modified: February 21st, 2019 [12:40am]
 * @Version:  1.0.0
 *
 * Copyright (C) 2019 by Ben Sokol. All Rights Reserved.
@@ -13,13 +13,18 @@
 #define TS_CALL_HPP
 
 #include <mutex>
-#include <shared_mutex>
 
 namespace TS {
   template <typename mtx_type, typename return_type, typename... Args>
   return_type call(mtx_type &mtx, return_type (*func)(Args...), Args... args) {
     std::lock_guard<std::mutex> lg(mtx);
     return func(args...);
+  }
+
+  template <typename mtx_type, typename... Args>
+  void call(mtx_type &mtx, void (*func)(Args...), Args... args) {
+    std::lock_guard<std::mutex> lg(mtx);
+    func(args...);
   }
 }  // namespace TS
 
--- TS_print.hpp
+++ TS_print.hpp
@@ -3,7 +3,7 @@
 * @Author:   Ben Sokol <Ben>
 * @Email:    ben@bensokol.com
 * @Created:  February 15th, 2019 [2:36pm]
-* @Modified: February 16th, 2019 [9:07pm]
+* @Modified: February 21st, 2019 [12:40am]
 * @Version:  1.0.0
 *
 * Copyright (C) 2019 by Ben Sokol. All Rights Reserved.
@@ -14,18 +14,17 @@
 
 #include <iostream>
 #include <mutex>
-#include <shared_mutex>
 
 namespace TS {
   namespace PRIVATE {
     template <typename T>
     void print(T t) {
-      std::cout << t;
+      std::cout << t << std::flush;
     }
 
     template <typename T, typename... Args>
     void print(T t, Args... args) {
-      std::cout << t;
+      std::cout << t << std::flush;
       TS::PRIVATE::print(args...);
     }
   }  // namespace PRIVATE
@@ -33,14 +32,14 @@ namespace TS {
   template <typename mtx_type, typename T>
   void print(mtx_type& mtx, T t) {
     mtx.lock();
-    std::cout << t;
+    std::cout << t << std::flush;
     mtx.unlock();
   }
 
   template <typename mtx_type, typename T, typename... Args>
   void print(mtx_type& mtx, T t, Args... args) {
     mtx.lock();
-    std::cout << t;
+    std::cout << t << std::flush;
     TS::PRIVATE::print(args...);
     mtx.unlock();
   }
